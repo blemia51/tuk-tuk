81585fa147235022bac75ca29631b52924e52e62
blemia51 <50045129+blemia51@users.noreply.github.com>
2020-11-05 16:39:17 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require('./index')
 const port = process.env.port || 5000;
 
 
-app.listen(port, (err) => {
+app.listen(port, '0.0.0.0', (err) => {
   if (err) {
     throw new Error('Something bad happened...');
 	}
